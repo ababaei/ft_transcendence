8fc8005f32f5a5d724c5574882921ca934dcaf88
ababaei <kzmmaz@gmail.com>
2023-10-10 17:42:32 +0200

    merging chatwithcomponent in main

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,15 +13,9 @@ export default defineConfig({
         target: 'http://backend:3000',
         ws: true,
         changeOrigin: true,
-<<<<<<< HEAD
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       }
-=======
-        secure:false,
-        rewrite: (path) => path.replace(/^\/api/,"")
-    },
->>>>>>> 912d5fd6018a84bfb9cf00c6e63cf2f4f24d7a9c
     }
   },
   plugins: [
