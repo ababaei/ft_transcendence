f23793245c2748d633889b39826b3162efe9c8bb
caubry <caubry@student.42.fr>
2023-10-19 15:33:08 +0200

    relation player a relier aux scores

--- backend/src/players/players.service.ts
+++ backend/src/players/players.service.ts
@@ -1,35 +1,35 @@
-// import { Inject, Injectable } from "@nestjs/common";
-// import { Game, Player } from "@prisma/client";
-// import { PrismaService } from "src/prisma/prisma.service";
+import { Inject, Injectable } from "@nestjs/common";
+import { Game, Player } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
 
-// export class PlayersService {
+export class PlayersService {
 
-//     constructor(@Inject(PrismaService)
-//         private prisma: PrismaService,
-//         ) {}
+    constructor(@Inject(PrismaService)
+        private prisma: PrismaService,
+        ) {}
 
-//     async getPlayers(id: number): Promise<Player[]>{
-//         return this.prisma.player.findMany({
-//             where: {gameID:id}
-//         })
-//     }
+    async getPlayers(id: number): Promise<Player[]>{
+        return this.prisma.player.findMany({
+            where: {gameID:id}
+        })
+    }
 
-//     async getPlayer(socket: string): Promise<Player | null>{
-//         return this.prisma.player.findUnique({where: {socket:socket}})
-//     }
-
-    // async createPlayer(data: Player): Promise<Player>{
-    //     return this.prisma.player.create({
-    //         data
-    //     })
+    // async getPlayer(socket: string): Promise<Player | null>{
+    //     return this.prisma.player.findUnique({where: {socket:socket}})
     // }
 
-    // async updatePlayer(player: Player, socket: string): Promise<Player>{
-    //     return this.prisma.player.update({
-    //         where: {id:player.id},
-    //         data: {socket:socket}
-    //     })
-    // }
+    async createPlayer(data: Player): Promise<Player>{
+        return this.prisma.player.create({
+            data
+        })
+    }
+
+    async updatePlayer(player: Player, score: number): Promise<Player>{
+        return this.prisma.player.update({
+            where: {id:player.id},
+            data: {score:score}
+        })
+    }
 
     // async newPlayer(game: Game, socket: string): Promise<Player>{
     //     const newPlayer = await this.prisma.user.update({
@@ -54,4 +54,4 @@
     //     const nb = this.prisma.player.count();
     //     return nb;
     // }
-// }
+}
